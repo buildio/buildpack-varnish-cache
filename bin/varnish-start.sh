d5bdfd65998857d5b6b5310f11458f95c52205a6
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-09-14 23:56:58 +0900

    Fix if/else error.

--- bin/varnish-start.sh
+++ bin/varnish-start.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
+# Function to check if a port is in use
+is_port_in_use() {
+  (echo > /dev/tcp/127.0.0.1/$1) &>/dev/null
+}
+
+
 # If /tmp/varnish already exists, this script has already been run
 if [ -d /tmp/varnish ]; then
   # This is awful, but we cannot call exit here because it will kill the dyno
+  echo "/tmp/varnish already exists. Exiting script."
+  return 0  # Use exit 0 if it's a standalone script
 else
-  # Function to check if a port is in use
-  is_port_in_use() {
-    (echo > /dev/tcp/127.0.0.1/$1) &>/dev/null
-  }
-
   PORT=${PORT:-8080}
 
   # Find an available port for the app
   APP_PORT=8080  # Starting with 8080
+  MAX_PORT=65535  # Maximum available port
   while is_port_in_use $APP_PORT || [ "$APP_PORT" -eq "$PORT" ]; do
-      APP_PORT=$((APP_PORT + 1))
+    if [ "$APP_PORT" -ge "$MAX_PORT" ]; then
+      echo "No available ports found."
+      exit 1
+    fi
+    APP_PORT=$((APP_PORT + 1))
   done
 
   # Run Varnish on the Heroku-assigned port ($PORT) and forward traffic to $APP_PORT
