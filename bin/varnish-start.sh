cbf11476b7891ea250cc89d515d4794bb9dd4e2a
usiegl00 <50933431+usiegl00@users.noreply.github.com>
2024-09-14 23:36:41 +0900

    Cannot use exit in varnish-start.

--- bin/varnish-start.sh
+++ bin/varnish-start.sh
@@ -2,28 +2,28 @@
 
 # If /tmp/varnish already exists, this script has already been run
 if [ -d /tmp/varnish ]; then
-  exit 0
+  # This is awful, but we cannot call exit here because it will kill the dyno
+else
+  # Function to check if a port is in use
+  is_port_in_use() {
+    (echo > /dev/tcp/127.0.0.1/$1) &>/dev/null
+  }
+
+  PORT=${PORT:-8080}
+
+  # Find an available port for the app
+  APP_PORT=8080  # Starting with 8080
+  while is_port_in_use $APP_PORT || [ "$APP_PORT" -eq "$PORT" ]; do
+      APP_PORT=$((APP_PORT + 1))
+  done
+
+  # Run Varnish on the Heroku-assigned port ($PORT) and forward traffic to $APP_PORT
+  /app/vendor/varnish/sbin/varnishd -a :$PORT -b 127.0.0.1:$APP_PORT -n /tmp/varnish
+  # Output varnish logs to stdout
+  /app/vendor/varnish/bin/varnishncsa -n /tmp/varnish &
+
+  # Export the new port for the app to use
+  export PORT=$APP_PORT
+  # Remove the APP_PORT variable from the environment
+  unset APP_PORT
 fi
-
-# Function to check if a port is in use
-is_port_in_use() {
-  (echo > /dev/tcp/127.0.0.1/$1) &>/dev/null
-}
-
-PORT=${PORT:-8080}
-
-# Find an available port for the app
-APP_PORT=8080  # Starting with 8080
-while is_port_in_use $APP_PORT || [ "$APP_PORT" -eq "$PORT" ]; do
-    APP_PORT=$((APP_PORT + 1))
-done
-
-# Run Varnish on the Heroku-assigned port ($PORT) and forward traffic to $APP_PORT
-/app/vendor/varnish/sbin/varnishd -a :$PORT -b 127.0.0.1:$APP_PORT -n /tmp/varnish
-# Output varnish logs to stdout
-/app/vendor/varnish/bin/varnishncsa -n /tmp/varnish &
-
-# Export the new port for the app to use
-export PORT=$APP_PORT
-# Remove the APP_PORT variable from the environment
-unset APP_PORT
